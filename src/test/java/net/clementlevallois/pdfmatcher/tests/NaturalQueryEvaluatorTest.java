2157a5e6e61a99704cb226659e4032a56b46059a
seinecle <1244100+seinecle@users.noreply.github.com>
2023-07-04 14:20:01 +0200

    documented the test a bit more

--- src/test/java/net/clementlevallois/pdfmatcher/tests/NaturalQueryEvaluatorTest.java
+++ src/test/java/net/clementlevallois/pdfmatcher/tests/NaturalQueryEvaluatorTest.java
@@ -1,6 +1,5 @@
 package net.clementlevallois.pdfmatcher.tests;
 
-
 import net.clementlevallois.pdfmatcher.controller.NaturalQueryEvaluator;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,20 +8,20 @@ import org.junit.Test;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author LEVALLOIS
  */
 public class NaturalQueryEvaluatorTest {
-    
-    
+
     @Test
     public void testPdfmatcher() {
         String query = """
+
                        "java" OR (python AND programming) AND NOT("monty python")
+
                        """;
-        Boolean caseSensitive = false; 
+        Boolean caseSensitive = false;
         NaturalQueryEvaluator ev = new NaturalQueryEvaluator();
         ev.setQueryInNaturalLanguage(query, caseSensitive);
         Boolean matched1 = ev.evaluate("test with Python programming");
@@ -31,16 +30,24 @@ public class NaturalQueryEvaluatorTest {
         Assert.assertTrue(matched1);
         Assert.assertFalse(matched2);
 
+        /*
+        this is an intentionally INCORRECT query,
+        where the opening double quote is NOT paired with a closing double quote (as it should)
+        in this case, the evaluator is expected to return a null value
+         */
+        
+        
         query = """
                        "java OR (python AND programming) AND NOT("monty python")
-                       """;
-        caseSensitive = false; 
+
+                """;
+
+        caseSensitive = false;
         ev = new NaturalQueryEvaluator();
         ev.setQueryInNaturalLanguage(query, caseSensitive);
         matched1 = ev.evaluate("test with Python programming");
         Assert.assertNull(matched1);
 
-        
     }
 
 }
